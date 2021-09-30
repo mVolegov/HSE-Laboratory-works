acf2ebc80e9d33184f1992afecf6be100f544bd6
mvolegov <volegovmikhail12@gmail.com>
2021-09-30 08:32:28 +0500

    add lab1 lab2

---
+++ lab1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Лабораторная работа 1, вариант 3");
+
+
+                Console.WriteLine("\nЗадача 1:");
+
+                bool isValidInput = false;
+                double n;
+
+                do
+                {
+                    Console.Write("n?");
+                    isValidInput = double.TryParse(Console.ReadLine(), out n);
+                    if (!isValidInput) Console.WriteLine("Вы ввели некорректные данные! Попробуйте снова");
+                } while (!isValidInput);
+
+                double m;
+
+                do
+                {
+                    Console.Write("m?");
+                    isValidInput = double.TryParse(Console.ReadLine(), out m);
+                    if (!isValidInput) Console.WriteLine("Вы ввели некорректные данные! Попробуйте снова");
+                } while (!isValidInput);
+
+                Console.WriteLine($"m={m} n={n} m--n={m-- - n}");
+                Console.WriteLine($"m={m} n={n} m++<n={m++ < n}");
+                Console.WriteLine($"m={m} n ={n} n++>m={n++ > m}");
+
+                double x;
+
+                do
+                {
+                    Console.Write("x?");
+                    isValidInput = double.TryParse(Console.ReadLine(), out x);
+                    if (!isValidInput)
+                    {
+                        Console.WriteLine("Вы ввели некорректные данные! Попробуйте снова");
+                    }
+                    else if (x > 1 || x < -1)
+                    {
+                        Console.WriteLine("Область определения y = arcsin(x): -1 <= x <= 1");
+                        isValidInput = false;
+                    }
+                } while (!isValidInput);
+
+                Console.WriteLine($"x={x} x^4 - cos(arcsin(x))={Math.Pow(x, 4) - Math.Cos(Math.Asin(x)):0.0000}");
+
+
+                Console.WriteLine("\nЗадача 2:");
+
+                double xPoint;
+                double yPoint;
+
+                do
+                {
+                    Console.Write("x?");
+                    isValidInput = double.TryParse(Console.ReadLine(), out xPoint);
+                    if (!isValidInput) Console.WriteLine("Вы ввели некорректные данные! Попробуйте снова");
+                } while (!isValidInput);
+
+                do
+                {
+                    Console.Write("y?");
+                    isValidInput = double.TryParse(Console.ReadLine(), out yPoint);
+                    if (!isValidInput) Console.WriteLine("Вы ввели некорректные данные! Попробуйте снова");
+                } while (!isValidInput);
+
+                Console.WriteLine($"Результат={(Math.Abs(xPoint) / 2 + Math.Abs(yPoint) / 2) <= 1}");
+
+
+                Console.WriteLine("\nЗадача 3:");
+
+                const int A = 100;
+                const double B = 0.001;
+
+                double a1Double = Math.Pow((A + B), 3);
+                double a2Double = Math.Pow(A, 3) + 3 * Math.Pow(A, 2) * B;
+                double a3Double = 3 * A * Math.Pow(B, 2);
+                double a4Double = Math.Pow(B, 2);
+                double resDouble = (a1Double - a2Double) / (a3Double + a4Double);
+                Console.WriteLine($"Результат для double={resDouble:0.00000000}");
+
+                float a1Float = (float)Math.Pow((A + B), 3);
+                float a2Float = (float)(Math.Pow(A, 3) + 3 * Math.Pow(A, 2) * B);
+                float a3Float = (float)(3 * A * Math.Pow(B, 2));
+                float a4Float = (float)Math.Pow(B, 2);
+                float resFloat = (a1Float - a2Float) / (a3Float + a4Float);
+                Console.WriteLine($"Результат для float={resFloat}");
+            }
+        }
+    }
+}
---
+++ lab2.cs
@@ -0,0 +1,156 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа 2, вариант 3");
+            
+            
+            Console.WriteLine("\nЗадача 1 (3)");
+
+            int nTask1;
+            double _a;
+            double sum = 0;
+            bool isValidInput;
+            
+            do
+            {
+                Console.Write("Введите n:");
+                isValidInput = int.TryParse(Console.ReadLine(), out nTask1);
+                
+                if (!isValidInput)
+                {
+                    Console.WriteLine("Вы ввели некорректные данные!");
+                }
+                else if (nTask1 < 0)
+                {
+                    Console.WriteLine("Количество элементов в последовательности не может быть отрицательным!");
+                    isValidInput = false;
+                } else if (nTask1 == 0)
+                {
+                    Console.WriteLine("Пустая последовательность!");
+                    isValidInput = false;
+                }
+            } while (!isValidInput);
+            
+            do
+            {
+                Console.Write($"a1=");
+                isValidInput = double.TryParse(Console.ReadLine(), out _a);
+
+                if (!isValidInput)
+                {
+                    Console.WriteLine("Вы ввели некорректные данные!");
+                }
+            } while (!isValidInput);
+            
+            for (int i = 2; i <= nTask1; i++)
+            {
+                do
+                {
+                    Console.Write($"a{i}=");
+                    isValidInput = double.TryParse(Console.ReadLine(), out _a);
+
+                    if (!isValidInput)
+                    {
+                        Console.WriteLine("Вы ввели некорректные данные!");
+                    }
+                } while (!isValidInput);
+
+                if (i % 2 == 0) sum += _a;
+            }
+            
+            Console.WriteLine($"Сумма элементов с четными номерами={sum:0.00}");
+
+            
+            Console.WriteLine("\nЗадача 2 (32)");
+
+            double _b;
+            int counter = 0;
+            int j = 1;
+            
+            do
+            {
+                Console.Write("1 число в последовательности=");
+                isValidInput = double.TryParse(Console.ReadLine(), out _b);
+
+                if (!isValidInput)
+                {
+                    Console.WriteLine("Вы ввели некорректные данные!");
+                }
+            } while (!isValidInput);
+
+            double firstElem = _b;
+            
+            while (_b != 0)
+            {
+                j++;
+                
+                do
+                {
+                    Console.Write($"{j} элемент в последовательности=");
+                    isValidInput = double.TryParse(Console.ReadLine(), out _b);
+
+                    if (!isValidInput)
+                    {
+                        Console.WriteLine("Вы ввели некорректные данные!");
+                    }
+                } while (!isValidInput);
+
+                if (_b % firstElem == 0 && _b != 0)
+                {
+                    counter++;
+                }
+            }
+            
+            Console.WriteLine($"Количество элементов, кратных первому элементу={counter}");
+            
+                
+            Console.WriteLine("\nЗадача 3 (37)");
+
+            int nTask3;
+            int itemCounter = 0;
+            int itemGenerator = 0;
+            int row = 0;
+
+            do
+            {
+                Console.Write("Введите n:");
+                isValidInput = int.TryParse(Console.ReadLine(), out nTask3);
+
+                if (!isValidInput)
+                {
+                    Console.WriteLine("Вы ввели некорректные данные!");
+                } else if (nTask3 < 0)
+                {
+                    Console.WriteLine("Количество элементов в последовательности не может быть отрицательным!");
+                    isValidInput = false;
+                } else if (nTask3 == 0)
+                {
+                    Console.WriteLine("Пустая последовательность!");
+                    isValidInput = false;
+                }
+            } while (!isValidInput);
+            
+            do
+            {
+                itemCounter++;
+                itemGenerator++;
+                
+                if (itemCounter % 3 == 0)
+                {
+                    row -= itemGenerator;
+                }
+                else
+                {
+                    row += itemGenerator;
+                }
+            } while (itemCounter < nTask3);
+            
+            Console.WriteLine($"Последовательность={row}");
+        }
+    }
+}
