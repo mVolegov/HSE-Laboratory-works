9736f98182d51d846bfa9137b1afd39fa0fe80fe
mvolegov <volegovmikhail12@gmail.com>
2021-10-03 20:15:46 +0500

    some changes in lab2.cs

--- lab2.cs
+++ lab2.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ConsoleApp1
 {
@@ -6,100 +6,42 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Лабораторная работа 2, вариант 3");
-            
-            
+            Console.WriteLine("\nЛабораторная работа 2, вариант 3");
+
+
             Console.WriteLine("\nЗадача 1 (3)");
-
-            int nTask1;
-            double _a;
-            double sum = 0;
-            bool isValidInput;
             
-            do
-            {
-                Console.Write("Введите n:");
-                isValidInput = int.TryParse(Console.ReadLine(), out nTask1);
-                
-                if (!isValidInput)
-                {
-                    Console.WriteLine("Вы ввели некорректные данные!");
-                }
-                else if (nTask1 < 0)
-                {
-                    Console.WriteLine("Количество элементов в последовательности не может быть отрицательным!");
-                    isValidInput = false;
-                } else if (nTask1 == 0)
-                {
-                    Console.WriteLine("Пустая последовательность!");
-                    isValidInput = false;
-                }
-            } while (!isValidInput);
+            int sum = 0;
             
-            do
-            {
-                Console.Write($"a1=");
-                isValidInput = double.TryParse(Console.ReadLine(), out _a);
-
-                if (!isValidInput)
-                {
-                    Console.WriteLine("Вы ввели некорректные данные!");
-                }
-            } while (!isValidInput);
+            int nTask1 = CheckInputSequenceLenght("n=");
+            
+            int _a = CheckInputElementInSequence("a1=");
             
             for (int i = 2; i <= nTask1; i++)
             {
-                do
-                {
-                    Console.Write($"a{i}=");
-                    isValidInput = double.TryParse(Console.ReadLine(), out _a);
-
-                    if (!isValidInput)
-                    {
-                        Console.WriteLine("Вы ввели некорректные данные!");
-                    }
-                } while (!isValidInput);
-
+                _a = CheckInputElementInSequence($"a{i}=");
+            
                 if (i % 2 == 0) sum += _a;
             }
             
-            Console.WriteLine($"Сумма элементов с четными номерами={sum:0.00}");
-
+            Console.WriteLine($"Сумма элементов с четными номерами={sum}");
+            
             
             Console.WriteLine("\nЗадача 2 (32)");
-
-            double _b;
+            
             int counter = 0;
             int j = 1;
             
-            do
-            {
-                Console.Write("1 число в последовательности=");
-                isValidInput = double.TryParse(Console.ReadLine(), out _b);
-
-                if (!isValidInput)
-                {
-                    Console.WriteLine("Вы ввели некорректные данные!");
-                }
-            } while (!isValidInput);
-
-            double firstElem = _b;
+            int _b = CheckInputElementInSequence("1 элемент в последовательности=");
+            
+            int firstElem = _b;
             
             while (_b != 0)
             {
                 j++;
-                
-                do
-                {
-                    Console.Write($"{j} элемент в последовательности=");
-                    isValidInput = double.TryParse(Console.ReadLine(), out _b);
-
-                    if (!isValidInput)
-                    {
-                        Console.WriteLine("Вы ввели некорректные данные!");
-                    }
-                } while (!isValidInput);
-
+            
+                _b = CheckInputElementInSequence($"{j} элемент в последовательности=");
+            
                 if (_b % firstElem == 0 && _b != 0)
                 {
                     counter++;
@@ -107,50 +49,82 @@ namespace ConsoleApp1
             }
             
             Console.WriteLine($"Количество элементов, кратных первому элементу={counter}");
+
             
-                
             Console.WriteLine("\nЗадача 3 (37)");
 
-            int nTask3;
-            int itemCounter = 0;
-            int itemGenerator = 0;
+            int item = 0;
             int row = 0;
+
+            int nTask3 = CheckInputSequenceLenght("n=");
+
+            Console.Write("S=");
 
             do
             {
-                Console.Write("Введите n:");
-                isValidInput = int.TryParse(Console.ReadLine(), out nTask3);
+                item++;
+
+                if (item % 3 == 0)
+                {
+                    row -= item;
+                    Console.Write("-" + item);
+                }
+                else
+                {
+                    row += item;
+                    Console.Write("+" + item);
+                }
+            } while (item < nTask3);
+
+            Console.WriteLine($"={row}");
+        }
+
+        static int CheckInputSequenceLenght(string message)
+        {
+            bool isValidInput;
+            int n;
+
+            do
+            {
+                Console.Write(message);
+                isValidInput = int.TryParse(Console.ReadLine(), out n);
 
                 if (!isValidInput)
                 {
                     Console.WriteLine("Вы ввели некорректные данные!");
-                } else if (nTask3 < 0)
+                }
+                else if (n < 0)
                 {
                     Console.WriteLine("Количество элементов в последовательности не может быть отрицательным!");
                     isValidInput = false;
-                } else if (nTask3 == 0)
+                }
+                else if (n == 0)
                 {
                     Console.WriteLine("Пустая последовательность!");
                     isValidInput = false;
                 }
             } while (!isValidInput);
-            
+
+            return n;
+        }
+
+        static int CheckInputElementInSequence(string message)
+        {
+            bool isValidInput;
+            int _element;
+
             do
             {
-                itemCounter++;
-                itemGenerator++;
-                
-                if (itemCounter % 3 == 0)
+                Console.Write(message);
+                isValidInput = int.TryParse(Console.ReadLine(), out _element);
+
+                if (!isValidInput)
                 {
-                    row -= itemGenerator;
+                    Console.WriteLine("Вы ввели некорректные данные!");
                 }
-                else
-                {
-                    row += itemGenerator;
-                }
-            } while (itemCounter < nTask3);
-            
-            Console.WriteLine($"Последовательность={row}");
+            } while (!isValidInput);
+
+            return _element;
         }
     }
 }
